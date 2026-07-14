98b938ab62ea0aef85f32a167400e2bf2b83a2f3
luisgs7 <luisgsilva270@gmail.com>
2026-07-13 21:39:47 -0300

    Add tests for comment creation and retrieval; implement fixtures for created comments and enhance post-related tests to include comments.

--- storeapi/tests/routers/test_post.py
+++ storeapi/tests/routers/test_post.py
@@ -12,6 +12,18 @@ async def created_post(async_client: AsyncClient):
     return await create_post(body="test post", async_client=async_client)
 
 
+@pytest.fixture()
+async def created_comment(async_client: AsyncClient, created_post: dict) -> dict:
+    response = await async_client.post(
+        "/comment",
+        json={
+            "body": "test comment",
+            "post_id": created_post["id"],
+        },
+    )
+    return response.json()
+
+
 @pytest.mark.anyio
 async def test_create_post(async_client: AsyncClient):
     body = "test post"
@@ -32,3 +44,48 @@ async def test_get_all_posts(async_client: AsyncClient, created_post: dict):
 
     assert response.status_code == 200
     assert response.json() == [created_post]
+
+@pytest.mark.anyio
+async def test_create_comment(async_client: AsyncClient, created_post: dict):
+    body = "test comment"
+    response = await async_client.post(
+        "/comment",
+        json={
+            "body": body,
+            "post_id": created_post["id"],
+        },
+    )
+
+    assert response.status_code == 201
+    assert {
+        "id": 0,
+        "body": body,
+        "post_id": created_post["id"],
+    }.items() <= response.json().items()
+
+
+@pytest.mark.anyio
+async def test_get_comments_on_post(async_client: AsyncClient, created_post: dict, created_comment: dict):
+    response = await async_client.get(f"/post/{created_post['id']}/comment")
+    assert response.status_code == 200
+    assert response.json() == [created_comment]
+
+
+@pytest.mark.anyio
+async def test_get_post_with_comments(
+    async_client: AsyncClient,
+    created_post: dict,
+    created_comment: dict
+):
+    response = await async_client.get(f"/post/{created_post['id']}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "post": created_post,
+        "comments": [created_comment],
+    }
+
+
+@pytest.mark.anyio
+async def test_get_missing_post_with_comments(async_client: AsyncClient, created_post: dict, created_comment: dict):
+    response = await async_client.get(f"/post/{created_post['id'] + 1}")
+    assert response.status_code == 404
